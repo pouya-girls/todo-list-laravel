afe677c057c786b86065fc85b4b4ca3f5ba4f287
Mohammad Nourinik <mohammad@inpin.co>
2018-01-25 16:12:55 +0330

    Init Commit

---
+++ resources/views/first-view.blade.php
@@ -0,0 +1,21 @@
+<html>
+<head>
+    <title>Todo list</title>
+</head>
+<body>
+<h1>Todo list</h1>
+
+<ul>
+    <li>Item1</li>
+    <li>Item2</li>
+    <li>Item3</li>
+    <li>Item4</li>
+    <li>Item5</li>
+</ul>
+
+<form>
+    <input>
+    <button>Add</button>
+</form>
+</body>
+</html>
