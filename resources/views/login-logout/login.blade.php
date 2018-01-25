88ef23eb3b074c0050b85a00d83128eab6527b40
Mohammad Nourinik <mohammad@inpin.co>
2018-01-25 17:47:04 +0330

    Beautify login.blade.php

--- resources/views/login-logout/login.blade.php
+++ resources/views/login-logout/login.blade.php
@@ -1,17 +1,37 @@
 <html>
 <head>
     <title>Login</title>
+
+    <link rel="stylesheet" type="text/css" href="/css/app.css">
 </head>
 <body>
-<h1>Login</h1>
-<form>
-    <label>Email :</label>
-    <input name="email" placeholder="Email">
-    <br>
-    <label>Password :</label>
-    <input type="password" name="password" placeholder="Password">
-    <br>
-    <button type="submit">Login</button>
-</form>
+<div class="container">
+    <div class="panel" style="margin-top: 30px">
+        <div class="panel-heading">
+            <h1 class="text-center">Login</h1>
+        </div>
+        <div class="panel-body">
+            <form class="form-horizontal">
+                <div class="form-group">
+                    <label class=" col-sm-2 control-label">Email :</label>
+                    <div class="col-sm-10">
+                        <input name="email" placeholder="Email" class="form-control">
+                    </div>
+                </div>
+                <div class="form-group">
+                    <label class=" col-sm-2 control-label">Password :</label>
+                    <div class="col-sm-10">
+                        <input type="password" name="password" placeholder="Password" class="form-control">
+                    </div>
+                </div>
+                <div class="form-group">
+                    <div class="col-sm-push-2 col-sm-10">
+                        <button type="submit" class="btn btn-primary">Login</button>
+                    </div>
+                </div>
+            </form>
+        </div>
+    </div>
+</div>
 </body>
 </html>
