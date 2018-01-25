12d20567fac386873c03d7c12cef0727cf260c6e
Mohammad Nourinik <mohammad@inpin.co>
2018-01-25 16:38:06 +0330

    Create a login form.

---
+++ resources/views/login-logout/login.blade.php
@@ -0,0 +1,17 @@
+<html>
+<head>
+    <title>Login</title>
+</head>
+<body>
+<h1>Login</h1>
+<form>
+    <label>Email :</label>
+    <input name="email" placeholder="Email">
+    <br>
+    <label>Password :</label>
+    <input type="password" name="password" placeholder="Password">
+    <br>
+    <button type="submit">Login</button>
+</form>
+</body>
+</html>
